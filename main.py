f147d28624191012c126c91c7994d7944c2fbf48
hnorthcott <htnorthcott@wpi.edu>
2018-09-21 11:01:00 -0400

    esummary implementation- logging just authors names

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Bio import Entrez
+
 Entrez.email = 'awmoulaison@wpi.edu'
 Entrez.api_key = 'f513b4e2e1a0b578c9d3dd731e36f19f7f08'
+
 
 def __init__(self, email):
     """Entrez requires and email address."""
@@ -28,9 +30,25 @@ def fetch_details(id_list):
     return results
 
 
+def summary_details(id_list):
+    ids = ','.join(id_list)
+    Entrez.email = 'awmoulaison@wpi.edu'
+    handle = Entrez.esummary(db = 'pubmed',
+                             retmode = 'xml',
+                             id= ids)
+    records = Entrez.parse(handle)
+
+    for record in records:
+        print(record['AuthorList'])
+        print(record['LastAuthor'])
+
+
+
+
 if __name__ == '__main__':
-    results = search('fever')
+    results = search('cancer')
     id_list = results['IdList']
+    summary_details(id_list)
     papers = fetch_details(id_list)
     for i, paper in enumerate(papers['PubmedArticle']):
         print("%d) %s" % (i + 1, paper['MedlineCitation']['Article']['ArticleTitle']))
