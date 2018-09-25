d1fcc0cac65bd4206298315c9948a9e66edd76cc
hnorthcott <htnorthcott@wpi.edu>
2018-09-25 09:49:13 -0400

    Meeting Tuesday 9/25 Work

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ Entrez.api_key = 'f513b4e2e1a0b578c9d3dd731e36f19f7f08'
 
 
 def __init__(self, email):
-    """Entrez requires and email address."""
     Entrez.email = 'awmoulaison@wpi.edu'
     Entrez.api_key = 'f513b4e2e1a0b578c9d3dd731e36f19f7f08'
 
@@ -38,9 +37,22 @@ def summary_details(id_list):
                              id= ids)
     records = Entrez.parse(handle)
 
+
+    lastAuthors = []
     for record in records:
-        print(record['AuthorList'])
-        print(record['LastAuthor'])
+        #print(record['AuthorList'])
+        #print(record['LastAuthor'])
+        lastAuthors.append(record['LastAuthor'])
+    print(lastAuthors)
+
+    paperDict = {}
+    for record in records:
+        for author in lastAuthors:
+            if author in paperDict:
+                paperDict[author].append((record['AuthorList']).contains(author))
+            else:
+                paperDict[author] = (record['AuthorList']).contains(author)
+    print(paperDict)
 
 
 
@@ -50,8 +62,9 @@ if __name__ == '__main__':
     id_list = results['IdList']
     summary_details(id_list)
     papers = fetch_details(id_list)
-    for i, paper in enumerate(papers['PubmedArticle']):
-        print("%d) %s" % (i + 1, paper['MedlineCitation']['Article']['ArticleTitle']))
+    # for i, paper in enumerate(papers['PubmedArticle']):
+        # print("%d) %s" % (i + 1, paper['MedlineCitation']['Article']['ArticleTitle']))
+
 
 
 
